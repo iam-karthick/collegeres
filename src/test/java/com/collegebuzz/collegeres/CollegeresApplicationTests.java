1a25ed42840f8f063abbe419e83e2111d52bddbe
karthick <30967392+iam-karthick@users.noreply.github.com>
2023-03-25 18:43:49 +0800

    first commit

---
+++ src/test/java/com/collegebuzz/collegeres/CollegeresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegebuzz.collegeres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
